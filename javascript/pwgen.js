43e372ffb1344a6aeb17456bb251f7a319491a14
TheKingJaffa <KingJ@toke.com>
2016-12-18 14:07:49 +0000

    .

--- javascript/pwgen.js
+++ javascript/pwgen.js
@@ -1,18 +1,16 @@
-import random
-
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-upperalphabet = alphabet.upper()
-pw_len = 16
-pwlist = []
-
-for i in range(pw_len//3):
-    pwlist.append(alphabet[random.randrange(len(alphabet))])
-    pwlist.append(upperalphabet[random.randrange(len(upperalphabet))])
-    pwlist.append(str(random.randrange(10)))
-for i in range(pw_len-len(pwlist)):
-    pwlist.append(alphabet[random.randrange(len(alphabet))])
-
-random.shuffle(pwlist)
-pwstring = "".join(pwlist)
-
-print(pwstring)
+function randomPassword(length)
+{
+  chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+  pass = "";
+  for(x=0;x<length;x++)
+  {
+    i = Math.floor(Math.random() * 62);
+    pass += chars.charAt(i);
+  }
+  return pass;
+}
+function formSubmit()
+{
+  passform.passbox.value = randomPassword(passform.length.value);
+  return false;
+}
