099e97163caaed633bc270201a666d50d50d369a
TheKingJaffa <KingJ@toke.com>
2016-12-18 15:36:43 +0000

    .

--- javascript/pwgen.js
+++ javascript/pwgen.js
@@ -1,16 +1,16 @@
 function randomPassword(length)
 {
-chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!\"#$%&'()*+,-./:;<=>?[\]^_`{|}~";
-pass = "";
-for(x=0;x<length;x++)
+	chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+	pass = "";
+	for(x=0;x<length;x++)
 {
-i = Math.floor(Math.random() * 62);
-pass += chars.charAt(i);
+	i = Math.floor(Math.random() * 62);
+	pass += chars.charAt(i);
 }
-return pass;
+	return pass;
 }
 function formSubmit()
 {
-passform.passbox.value = randomPassword(passform.length.value);
-return false;
+	passform.passbox.value = randomPassword(passform.length.value);
+	return false;
 }
