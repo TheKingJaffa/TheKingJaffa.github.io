a0cb1950d623bba04aa96999c4e9ab99b31fb832
TheKingJaffa <KingJ@toke.com>
2017-01-12 16:28:49 +0000

    .

--- javascript/amsite.js
+++
@@ -1,8 +0,0 @@
-// AsciiMorph - made by Tim Holman, tholman.com
-
-// Set up Initial Morpher
-var morphElement = document.querySelectorAll('pre')[0];
-AsciiMorph(morphElement, {x: 51,y: 28});
-AsciiMorph.render(asciis[0]);
-
-var currentlyRendered = 0;
